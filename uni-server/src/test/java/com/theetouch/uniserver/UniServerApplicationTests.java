a6cbeb2ec2358a5ec704d75f6bb84027c775b3d7
theetouch-k <theetouch.k@gmail.com>
2023-03-06 22:39:57 +1100

    Initial code for server and client side

---
+++ uni-server/src/test/java/com/theetouch/uniserver/UniServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theetouch.uniserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
